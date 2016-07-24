cd97461062e12790c8d62427949cdda9402f962c
CoriolisBoy <coriolisboy@gmail.com>
2016-07-24 12:53:06 +0930

    Navmesh and AI

---
+++ Assets/Leader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Leader : MonoBehaviour {
+
+    public AICharacterControl follower;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+            follower.SetTarget(transform);
+	}
+}
