22672de3df4db1c9a2013842e36f7545fa019634
Dr_Mike_Cooper <coriolisboy@gmail.com>
2016-10-05 13:45:10 +1030

    Updated for VR

---
+++ Assets/GazeFollower.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeFollower : MonoBehaviour {
+
+    public Transform[] indicators;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Quaternion q = UnityEngine.VR.InputTracking.GetLocalRotation(UnityEngine.VR.VRNode.Head);
+        Vector3 dir = q * Camera.main.transform.forward;
+
+        foreach (Transform t in indicators)
+            t.up = dir;
+
+        Ray ray = new Ray(Camera.main.transform.position, dir);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000.0f))
+        {
+            transform.position = hit.point;
+        }
+    }
+}
+
