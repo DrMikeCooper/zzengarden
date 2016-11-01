f0188500e321bc77bc76841dc86d103a86e817c0
Dr_Mike_Cooper <coriolisboy@gmail.com>
2016-11-01 17:06:09 +1030

    Rails navigation and travel

--- Assets/GazeFollower.cs
+++ Assets/GazeFollower.cs
@@ -4,25 +4,28 @@ using System.Collections;
 public class GazeFollower : MonoBehaviour {
 
     public Transform[] indicators;
+    public bool contact;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         Quaternion q = UnityEngine.VR.InputTracking.GetLocalRotation(UnityEngine.VR.VRNode.Head);
-        Vector3 dir = q * Camera.main.transform.forward;
+        Vector3 dir = Camera.main.transform.forward;
 
         foreach (Transform t in indicators)
             t.up = dir;
 
         Ray ray = new Ray(Camera.main.transform.position, dir);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 1000.0f))
+        contact = false;
+        if (Physics.Raycast(ray, out hit, 1000.0f, (1<<8)|(1<<9)))
         {
+            contact = (hit.transform.gameObject.layer == 8);
             transform.position = hit.point;
         }
     }
---
+++ Assets/RailTraveller.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RailTraveller : MonoBehaviour {
+
+    public GazeFollower gaze;
+    Vector3 targetPosition;
+    Vector3 gazeStart;
+    public float timer = 0;
+    public bool contact;
+    NavMeshAgent nv;
+
+    // Use this for initialization
+    void Start () {
+        targetPosition = transform.position;
+        gazeStart = targetPosition;
+
+        nv = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        contact = gaze.contact; //dbg
+        if (gaze.contact)
+        {
+            float distance = (gazeStart - gaze.transform.position).magnitude;
+            if (distance < 1.0f)
+                timer += Time.deltaTime;
+            else
+            {
+                gazeStart = gaze.transform.position;
+                timer = 0;
+            }
+            if (timer > 1)
+            {
+                nv.SetDestination(gaze.transform.position);
+                targetPosition = gaze.transform.position;
+                timer = 0;
+            }
+        }
+        else
+            timer = 0;
+
+        float sc = 0.2f *(1+ timer);
+        gaze.transform.localScale = new Vector3(sc,sc,sc);
+        //nv.Set
+        //transform.position = Vector3.MoveTowards(transform.position, targetPosition, 0.1f);
+	}
+}
